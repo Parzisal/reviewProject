ec42aae6a03aa29456a05a0c7e8b4085d130fd4a
Журавский Андрей Николаевич <ext.azhuravskiy@tinkoff.ru>
2024-06-25 20:55:02 +0300

    upd

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, CommonModule, ReactiveFormsModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {
+  title = 'book-management';
+}
